688dbb56a01e7fd7ceb862c9381bbd9516d91c17
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-19 21:43:12 -0500

    saving records

--- public/indexDb.js
+++ public/indexDb.js
@@ -8,3 +8,42 @@ request.onupgradeneeded = function (event) {
   };
   function checkDatabase() {
   }
+  request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+  request.onerror = function (event) {
+    console.log('There has been an error with retrieving your data: ' + request.error);
+  };
+  function saveRecord(record) {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const budgetStore = transaction.objectStore('budget');
+    budgetStore.add(record);
+  }
+  function checkDatabase() {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const budgetStore = transaction.objectStore('budget');
+    const getRequest = budgetStore.getAll();
+  
+    getRequest.onsuccess = function () {
+      if (getRequest.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getRequest.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(() => {
+            const transaction = db.transaction(['budget'], 'readwrite');
+            const budgetStore = transaction.objectStore('budget');
+            budgetStore.clear();
+          });
+      }
+    };
+  }
+  window.addEventListener('online', checkDatabase);
